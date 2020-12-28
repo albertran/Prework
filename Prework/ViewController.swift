937c24cec73882104b76d87759874a8c64579631
Albert Tran <alberdt2@uci.edu>
2020-12-27 17:06:14 -0800

    Initial Commit

---
+++ Prework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Prework
+//
+//  Created by Albert Tran on 12/27/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
